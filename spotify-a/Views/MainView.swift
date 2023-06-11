f925545b0789fce9422fe041fe795f825a41c353
Javier Aponte <31341776+javier-aponte@users.noreply.github.com>
2023-06-10 23:09:19 -0500

    first commit

---
+++ spotify-a/Views/MainView.swift
@@ -0,0 +1,29 @@
+//
+//  MainView.swift
+//  spotify-a
+//
+//  Created by Linder Anderson Hassinger Solano    on 26/05/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            SearchView()
+                .tabItem {
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+        }.accentColor(.white)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
